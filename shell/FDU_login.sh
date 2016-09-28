71d8152226a074e4b9f10ffb8ea0240cc9f68d9f
wenfengshi <wuzhimang@gmail.com>
2016-09-28 14:07:58 +0800

    modified

--- shell/FDU_login.sh
+++ shell/FDU_login.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 read -p "input number: " name
-#name=15210240000
 read -s -p "input password: " password
 pass=`echo -n "$password"|od -An -tx1 | tr ' ' %`
-echo "" #for new line
-wget http://10.108.255.249/cgi-bin/do_login --post-data "username=$name&password={TEXT}$pass&drop=0&type=1&n=100"i -q
-echo "" #for new line
+echo 
+wget http://10.108.255.249/cgi-bin/do_login --post-data "username=$name&password={TEXT}$pass&drop=0&type=1&n=100"i -q -O /tmp/login.log
+result=`cut /tmp/login.log -f 1`
+if grep -q '^[[:digit:]]*$' <<< "$result"
+then
+    echo -e "\nSucceed!"
+else
+    echo -e "\nFailed!\t$result"
+fi
+
